924f8826ad953b5f20bb7e5acbb4b04735e9340f
Payton Garland <payton.r.g@gmail.com>
2017-08-28 13:56:22 -0500

    Add all entries from EPS Directory to EPS tests

--- Tests/com/drew/metadata/eps/EpsReaderTest.java
+++ Tests/com/drew/metadata/eps/EpsReaderTest.java
@@ -9,6 +9,7 @@ import java.io.FileInputStream;
 import java.io.InputStream;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 
 /**
@@ -38,11 +39,18 @@ public class EpsReaderTest
     {
         EpsDirectory directory = processBytes("Tests/Data/8x4x8bit-Grayscale.eps");
 
+        assertFalse(directory.hasErrors());
+
         assertEquals(4334, directory.getInt(EpsDirectory.TAG_TIFF_PREVIEW_SIZE));
         assertEquals(30, directory.getInt(EpsDirectory.TAG_TIFF_PREVIEW_OFFSET));
+        assertEquals("Adobe Photoshop Version 2017.1.1 20170425.r.252 2017/04/25:23:00:00 CL 1113967", directory.getString(EpsDirectory.TAG_CREATOR));
+        assertEquals("8x4x8bit-Grayscale.eps", directory.getString(EpsDirectory.TAG_TITLE));
+        assertEquals("0 0 2 1", directory.getString(EpsDirectory.TAG_BOUNDING_BOX));
+        assertEquals("8 4 8 1 0 1 3 \"beginimage\"", directory.getString(EpsDirectory.TAG_IMAGE_DATA));
         assertEquals(8, directory.getInt(EpsDirectory.TAG_IMAGE_WIDTH));
         assertEquals(4, directory.getInt(EpsDirectory.TAG_IMAGE_HEIGHT));
         assertEquals("Grayscale", directory.getString(EpsDirectory.TAG_COLOR_TYPE));
+        assertEquals(32.0, directory.getFloat(EpsDirectory.TAG_RAM_SIZE), 0);
     }
 
     @Test
@@ -50,10 +58,17 @@ public class EpsReaderTest
     {
         EpsDirectory directory = processBytes("Tests/Data/adobeJpeg1.eps");
 
+        assertFalse(directory.hasErrors());
+
         assertEquals(41802, directory.getInt(EpsDirectory.TAG_TIFF_PREVIEW_SIZE));
         assertEquals(30, directory.getInt(EpsDirectory.TAG_TIFF_PREVIEW_OFFSET));
+        assertEquals("Adobe Photoshop Version 2017.1.1 20170425.r.252 2017/04/25:23:00:00 CL 1113967", directory.getString(EpsDirectory.TAG_CREATOR));
+        assertEquals("adobeJpeg1.eps", directory.getString(EpsDirectory.TAG_TITLE));
+        assertEquals("0 0 196 148", directory.getString(EpsDirectory.TAG_BOUNDING_BOX));
+        assertEquals("275 207 8 3 0 1 3 \"beginimage\"", directory.getString(EpsDirectory.TAG_IMAGE_DATA));
         assertEquals(275, directory.getInt(EpsDirectory.TAG_IMAGE_WIDTH));
         assertEquals(207, directory.getInt(EpsDirectory.TAG_IMAGE_HEIGHT));
         assertEquals("RGB", directory.getString(EpsDirectory.TAG_COLOR_TYPE));
+        assertEquals(170775.0, directory.getFloat(EpsDirectory.TAG_RAM_SIZE), 0);
     }
 }
